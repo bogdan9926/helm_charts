5f3284ffa33f2b0d172726df00f67f86eb04d925
Bogdan Popescu <30412621+bogdan9926@users.noreply.github.com>
2023-10-16 11:12:49 +0200

    Delete runHelm.sh

--- runHelm.sh
+++
@@ -1,9 +0,0 @@
-kubectl apply -f envmap.yaml
-kubectl apply -f  secretmap.yaml
-helm install ingress-nginx oci://ghcr.io/nginxinc/charts/nginx-ingress --version 1.0.0
-helm upgrade --install release corpool/. --set test-app-api.image.tag="0.1.0"   --set test-app-service.image.tag="0.1.0"   --debug 
-
-helm install mongo -f corpool/mongoValues.yaml oci://registry-1.docker.io/bitnamicharts/mongodb
-
-helm install rabbitmq -f corpool/rabbitValues.yaml oci://registry-1.docker.io/bitnamicharts/rabbitmq
-helm install redis -f corpool/redisValues.yaml oci://registry-1.docker.io/bitnamicharts/redis
